d17628f5bce0223e5c14dc002024135a7538beee
mbenedict1822 <mbenedict@college.harvard.edu>
2019-12-04 21:32:26 +0000

    Trying ports

--- application.py
+++ application.py
@@ -3,6 +3,16 @@ from flask_session import Session
 from tempfile import mkdtemp
 from cs50 import SQL
 
+### Weird internet port code
+
+import os
+#from intro_to_flask import app
+
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
+
+###
+
 app = Flask(__name__)
 
 app.config["SESSION_FILE_DIR"] = mkdtemp()
